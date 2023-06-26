12c9c8c69f2331c2935acf2588b3ef8866bb4085
Oluwakemi Richard <52783711+Oluwakemi-Richard@users.noreply.github.com>
2023-06-27 00:13:12 +0100

    registration and log in forms

---
+++ routes/accountRoute.js
@@ -0,0 +1,26 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const accountController = require("../controllers/accountController")
+const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
+
+//Deliver login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+//Register route
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+
+// router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
+
+
+
+module.exports = router;
